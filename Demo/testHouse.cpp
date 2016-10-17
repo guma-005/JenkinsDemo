953695ea2bb6de797e0d1c0f88b2ae9ed7146b3a
guma-005 <guma.005@gmail.com>
2016-10-17 17:39:37 +0200

    Update testHouse.cpp

--- Demo/testHouse.cpp
+++ Demo/testHouse.cpp
@@ -11,26 +11,26 @@
  * Created on October 17, 2016, 10:26 AM
  */
 
-#include "testHouse.hpp"
-#define BOOST_TEST_MODULE MyTest
 
-//#include "boost/test/unit_test.hpp"
+#define BOOST_TEST_MODULE dummy
+#include "testHouse.hpp"
+
 #define BOOST_TEST_MAIN
-#define BOOST_TEST_STATIC_LINK
-#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
 
 
 #include <boost/test/auto_unit_test.hpp>
 #include <boost/test/unit_test.hpp>
 
+
+
 BOOST_AUTO_TEST_SUITE(MasterSuite)
+
 BOOST_AUTO_TEST_CASE(test1)
 {
-    cout<<" Entered into test class "<<endl;
-    
+    cout<<" Entered into test class "<<endl;    
     Bed myBed;
-    myBed.bedSize = 10;
-   
+    myBed.bedSize = 10;   
     int size = 15;
     //BOOST_CHECK( test_object.is_valid() );
     BOOST_CHECK_EQUAL(myBed.getBedSize(), size);
