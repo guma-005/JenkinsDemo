026d2570f181fc01456d29274f8b823a6c8a1451
guma-005 <guma.005@gmail.com>
2016-10-17 16:09:15 +0200

    Add two test fils by uploading

---
+++ Demo/testHouse.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   testHouse.cpp
+ * Author: Uma
+ * 
+ * Created on October 17, 2016, 10:26 AM
+ */
+
+#include "testHouse.hpp"
+#define BOOST_TEST_MODULE MyTest
+
+//#include "boost/test/unit_test.hpp"
+
+
+#define BOOST_AUTO_TEST_MAIN
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(MasterSuite)
+BOOST_AUTO_TEST_CASE(test1)
+{
+    cout<<" Entered into test class "<<endl;
+    
+    Bed myBed;
+    myBed.bedSize = 10;
+   
+    int size = 15;
+    //BOOST_CHECK( test_object.is_valid() );
+    BOOST_CHECK_EQUAL(myBed.getBedSize(), size);
+}
+BOOST_AUTO_TEST_SUITE_END()
---
+++ Demo/testHouse.hpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   testHouse.hpp
+ * Author: Uma
+ *
+ * Created on October 17, 2016, 10:26 AM
+ */
+
+#ifndef TESTHOUSE_HPP
+#define TESTHOUSE_HPP
+#include "Bed.hpp"
+#include <iostream>
+using namespace std;
+class testHouse {
+public:
+    testHouse();
+    testHouse(const testHouse& orig);
+    virtual ~testHouse();
+private:
+
+};
+
+#endif /* TESTHOUSE_HPP */
+
